e9662275336ca6c602f46192605438c0ccfa1173
ertert1 <34750986+ertert1@users.noreply.github.com>
2018-05-08 20:32:21 +0500

    Create MathForm.java

---
+++ src/main/java/kr/KR/MathForm.java
@@ -0,0 +1,12 @@
+/**
+ * объявление пакета
+ */
+package kr.KR;
+/**
+ * Библиотеки необходимые для работы
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
