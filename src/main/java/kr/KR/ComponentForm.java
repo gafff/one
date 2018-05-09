c4e82a963f837b105f8b76dce6bf92be009b16a4
Amur21 <34628045+Amur21@users.noreply.github.com>
2018-05-09 14:30:21 +0500

    Update ComponentForm.java

--- src/main/java/kr/KR/ComponentForm.java
+++ src/main/java/kr/KR/ComponentForm.java
@@ -1,4 +1,39 @@
 /**
+ * объявление пакета
+ */
+package kr.KR;
+/**
+ * Библиотеки необходимые для работы
+ */
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+/**
+ * Объявление публичного класса для создания окна 
+ */
+public class ComponentForm extends JFrame {
+	/**
+	 * Описание компонентов формы
+	 */
+	JPanel contentPane;
+	JPanel Panel1;
+	JPanel Panel2;
+	static JLabel label_1;
+	static JLabel label_2;
+	static JLabel label_3;
+	static JTextField TxtBox1; 
+	static JTextField TxtBox2;
+	public static JFrame frame;
+	static JButton btnNewButton;
+/**
 	 * Объявление метода, в котором объявляются элементы экранной формы
 	 * SuppressWarnings-это объявление аннотации для отключения предупреждений об ошибках
 	 * Значок собаки может использоваться и для вызова индентифицирующих методов, для инициализации потоковых ресурсов
