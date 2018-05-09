1347b006218798f60e7f689d32422e07d0c06380
Amur21 <34628045+Amur21@users.noreply.github.com>
2018-05-09 14:35:05 +0500

    Update MathForm.java

--- src/main/java/kr/KR/MathForm.java
+++ src/main/java/kr/KR/MathForm.java
@@ -10,3 +10,25 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+/**
+ * Объявление публичного класса для создания зависимости между классами и выполнение вычислений 
+ */
+public class MathForm implements ActionListener {
+	/**
+	 * Переменные отвечающие за вычисление:
+	 *k1-Вводимая максимальная мощность
+	 *r1-Заранее заданное напряжение сети
+	 *result-Результат вычислений
+	 */
+	float k1;
+	float r1;
+	float result;
+	/**
+	 * Установление связи между родительским и дочерним классом
+	 */
+	MathForm(ComponentForm parent){
+    }
+	/**
+	 * Событие для кнопки "Рассчитать", присутствует обработка ошибок.
+	 */
+	public void actionPerformed(ActionEvent e)  {
